be4ce6c9e78cf9732d52c080e7d2c54e3b1727b2
Hang Luo <819776550@qq.com>
2024-06-18 19:02:38 +0800

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,8 +1,8 @@
 import numpy as np
-from methods.statistics_based.gray_pixels_qian import gray_pixels
+from statistics_based.gray_pixels_qian import gray_pixels
 import imageio
 
-img = imageio.imread('00_0011.png', 'PNG-FI').astype(np.float32)
+img = imageio.imread('example.png', 'PNG-FI').astype(np.float32)
 mask = np.sum(img, axis=2) != 0
-tmp = gray_pixels(img, mask = None)
-print(tmp)
+est_illu = gray_pixels(img, mask = None)
+print(est_illu)
